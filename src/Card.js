3877b651411dfc004c9e137f8767c50b85d28ce5
FAZ KAY <fkydln@gmail.com>
2021-11-17 22:43:38 -0800

    card details added

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+// import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img alt="robot selfie" src="https://robohash.org/faz1?200x200" />
+      <div>
+        <h2>RoboFaz</h2>
+        <p>jane.doe@doemail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
