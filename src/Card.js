c1033a9bf479b1983ca4c9aa91dd9a1aa7f890aa
FAZ KAY <fkydln@gmail.com>
2021-11-17 23:30:15 -0800

    adding robofaz2

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = () => {
     <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="robot selfie" src="https://robohash.org/faz1?200x200" />
       <div>
-        <h2>RoboFaz</h2>
+        <h2>RoboFaz2</h2>
         <p>jane.doe@doemail.com</p>
       </div>
     </div>
